15671aa16008582e061e416589d40eeba4bf9328
barbarossuplementossp <barbarossuplementossp@gmail.com>
2026-07-04 19:16:31 -0300

    Force new hash

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: { rollupOptions: { output: { entryFileNames: 'assets/[name]-[hash]-v2.[ext]' } } }
 })
